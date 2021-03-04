e17c79ecb18abc9783bef824b3734726ad7fb862
易成杰 <626659321@qq.com>
2021-03-04 22:39:34 +0800

    保存

--- web-mvc-study/src/main/java/com/yicj/mvc/WebMvcApplication.java
+++ web-mvc-study/src/main/java/com/yicj/mvc/WebMvcApplication.java
@@ -1,11 +1,9 @@
 package com.yicj.mvc;
 
-import com.yicj.mvc.web.servlet.AsyncServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CharacterEncodingFilter;
 

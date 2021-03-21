2b7e2d973dc9110a47ad0b361cfbafc8be5fcb93
易成杰 <626659321@qq.com>
2021-03-21 21:36:41 +0800

    保存

--- web-security-study/src/main/java/com/yicj/web/security/SpringSecurityApplication.java
+++ web-security-study/src/main/java/com/yicj/web/security/SpringSecurityApplication.java
@@ -3,7 +3,11 @@ package com.yicj.web.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @EnableWebSecurity
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -11,5 +15,11 @@ public class SpringSecurityApplication {
     //https://www.cnblogs.com/yy3b2007com/p/12194142.html
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args) ;
+    }
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello world" ;
     }
 }
--- web-security-study/src/main/java/com/yicj/web/security/config/annotation/web/configuration/MyWebSecurityConfigurer.java
+++ web-security-study/src/main/java/com/yicj/web/security/config/annotation/web/configuration/MyWebSecurityConfigurer.java
@@ -32,6 +32,17 @@ public class MyWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+            .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+            .and()
+            .formLogin()
+            .and()
+            .httpBasic()
+            .and()
+            .sessionManagement()
+                .maximumSessions(1)
+        ;
     }
 }

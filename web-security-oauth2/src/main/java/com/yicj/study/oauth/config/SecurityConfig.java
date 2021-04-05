1f182dbd0a9cf3d0d33e90ea9243142745603f80
易成杰 <626659321@qq.com>
2021-04-05 20:11:47 +0800

    保存

--- web-security-oauth2/src/main/java/com/yicj/study/oauth/config/SecurityConfig.java
+++ web-security-oauth2/src/main/java/com/yicj/study/oauth/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.yicj.study.oauth.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,6 +14,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 //.and()
                 //.formLogin()
+                .and()
+                .exceptionHandling()
+                    .authenticationEntryPoint( (request, resp, authException) -> {
+                        resp.setStatus(HttpStatus.UNAUTHORIZED.value());
+                        resp.getWriter().write("please login");
+                    })
+                    .accessDeniedHandler( (req, resp, ex) -> {
+                        resp.setStatus(HttpStatus.FORBIDDEN.value());
+                        resp.getWriter().write("forbidden");
+                    })
                 .and()
                 .oauth2Login() ;
     }

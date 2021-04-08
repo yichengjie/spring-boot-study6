5f7b0be5d6cff1cae4e79bb229b916dac30c6ebc
易成杰 <626659321@qq.com>
2021-04-08 20:49:39 +0800

    保存

--- web-mvc-study2/src/main/java/com/yicj/study/mvc/controller/HelloController.java
+++ web-mvc-study2/src/main/java/com/yicj/study/mvc/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.yicj.study.mvc.controller;
 
+import com.yicj.study.mvc.model.MyData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,5 +16,15 @@ public class HelloController {
         Map<String, Object> map = new HashMap<>() ;
         map.put("name", "Yicj") ;
         return map ;
+    }
+
+    @GetMapping("/hello2")
+    @ModelAttribute("data")
+    public MyData hello2(){
+        return getMyData() ;
+    }
+
+    private MyData getMyData(){
+        return MyData.builder().firstName("yi").lastName("cj").build() ;
     }
 }
---
+++ web-mvc-study2/src/main/java/com/yicj/study/mvc/model/MyData.java
@@ -0,0 +1,11 @@
+package com.yicj.study.mvc.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MyData {
+    private String firstName ;
+    private String lastName ;
+}

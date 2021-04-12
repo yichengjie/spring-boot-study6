1fe497b3d0754e22b835ad8f771bc6574b056442
易成杰 <626659321@qq.com>
2021-04-12 16:16:52 +0800

    保存

---
+++ hello-world-web/src/main/java/com/yicj/study/web/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.yicj.study.web.controller;
+
+import com.yicj.enchancer.annotation.EnableMvcCommonHandler;
+import com.yicj.enchancer.servlet.mvc.annotation.ResultEnhancerTag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableMvcCommonHandler
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResultEnhancerTag(success = "删除用户成功", error = "删除用户失败")
+    public boolean hello(){
+        return true ;
+    }
+}

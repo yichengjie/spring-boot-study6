c4384ac6ada62434157f70f6959084720ab773a0
易成杰 <626659321@qq.com>
2021-04-25 19:40:38 +0800

    保存

--- lb-67-demo-netty-server/src/main/java/com/yicj/study/netty/server/NettyServer.java
+++ lb-67-demo-netty-server/src/main/java/com/yicj/study/netty/server/NettyServer.java
@@ -8,8 +8,6 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;

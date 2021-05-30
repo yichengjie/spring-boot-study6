7771ce64f5c71642802e830180805074a8813127
易成杰 <626659321@qq.com>
2021-05-30 21:05:37 +0800

    保存

---
+++ func-program/src/main/java/com/yicj/study/oom/RuntimeConstantPoolOOM.java
@@ -0,0 +1,20 @@
+package com.yicj.study.oom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * vm args:-verbose:gc -XX:PermSize=10m -XX:MaxPermSize=10m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=D:\\tmp
+ */
+public class RuntimeConstantPoolOOM {
+
+    public static void main(String[] args) {
+        // 使用List保持着常量池引用，避免Full GC回收常量池行为
+        List<String> list = new ArrayList<>() ;
+        //10MB的PermSize在integer范围内足够产生OOM了
+        int i =0 ;
+        while (true){
+            list.add(String.valueOf(i ++).intern());
+        }
+    }
+}

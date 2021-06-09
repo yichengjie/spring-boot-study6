89193feb2fed577c4e4c46c63b0e4d3d8364a384
yicj1 <yicj1@lenovo.com>
2021-06-09 18:23:56 +0800

    保存

--- wechat-hello/src/main/java/com/yicj/study/controller/WechatController.java
+++ wechat-hello/src/main/java/com/yicj/study/controller/WechatController.java
@@ -1,9 +1,15 @@
 package com.yicj.study.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.yicj.study.model.SendTemplateMessageVo;
+import com.yicj.study.model.SlotItemVo;
 import com.yicj.study.model.result.AccessToken;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -62,13 +68,9 @@ public class WechatController {
     }
 
     /**
-     * {
-     *     "access_token": "45_hy_Cw5vPRFoWd6ra0cvLYNdN7Skto5_zhgl9FFQR51muPULaFAsHqIPgoRiPn62AI5ciyY10LasTPlqvyqoOdw",
-     *     "expires_in": 7200,
-     *     "refresh_token": "45_IAiui3UXfJm6Qwb6aWNaj4pTXn66CcGPA800Lro9vkmxweB3-RlRDqnaMyndALXlUFJrrpwngiJLzfkt3y8CIw",
-     *     "openid": "obC9S6seDG_rYlg90eAk7Qofw5-A",
-     *     "scope": "snsapi_userinfo"
-     * }
+     * 微信重定向回来接口，通过code获取用户信息
+     * @param code
+     * @return
      */
     @GetMapping("/wechatLogin")
     public String wechatLogin(String code){
@@ -90,4 +92,36 @@ public class WechatController {
     }
 
 
+    @GetMapping("/sendTemplateMessage")
+    public String sendTemplateMessage(String accessToken){
+        String url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={ACCESS_TOKEN}";
+        String openid ="obC9S6seDG_rYlg90eAk7Qofw5-A" ;
+        String template_id = "HKvainS_QdHhR42BgpwTLwlRY4fNJusVarGWsg2k6YI" ;
+        String msgUrl = "http://www.baiduxfzx.com/gyj/?bd_vid=9784367128317145609" ;
+        String first = "hello world" ;
+        String keyword1 = "测试" ;
+        String keyword2 = "测试数据" ;
+        //{{first.DATA}} {{keyword1.DATA}} 报名链接{{keyword2.DATA}}
+        Map<String, SlotItemVo> slots = new HashMap<>() ;
+        slots.put("first", SlotItemVo.builder().value(first).color("#44bd32").build()) ;
+        slots.put("keyword1", SlotItemVo.builder().value(keyword1).color("#273c75").build()) ;
+        slots.put("keyword2", SlotItemVo.builder().value(keyword2).color("#273c75").build()) ;
+
+        SendTemplateMessageVo templateMessageVo = SendTemplateMessageVo.builder()
+                //.touser(Arrays.asList(openid))
+                .touser(openid)
+                .template_id(template_id)
+                .url(msgUrl)
+                .data(slots).build();
+        System.out.println(JSON.toJSON(templateMessageVo).toString());
+
+        Map<String, String> param = new HashMap<>() ;
+        param.put("ACCESS_TOKEN", accessToken) ;
+
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/x-www-form-urlencoded; charset=UTF-8");
+        headers.setContentType(type);
+        HttpEntity<String> requestEntity = new HttpEntity<>(JSON.toJSON(templateMessageVo).toString(),headers);
+        return restTemplate.postForObject(url, requestEntity, String.class, param);
+    }
 }

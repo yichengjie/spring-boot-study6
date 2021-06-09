155a495f77a227114138f12a041c43b7d0a676e2
yicj1 <yicj1@lenovo.com>
2021-06-09 17:57:09 +0800

    保存

--- wechat-hello/src/main/java/com/yicj/study/controller/WechatController.java
+++ wechat-hello/src/main/java/com/yicj/study/controller/WechatController.java
@@ -4,11 +4,15 @@ import com.yicj.study.model.result.AccessToken;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,6 +33,7 @@ public class WechatController {
     @Autowired
     private RestTemplate restTemplate ;
 
+    // 获取AccessToken接口
     @GetMapping("/fetchAccessToken")
     public AccessToken fetchAccessToken(HttpServletResponse response){
         //https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET
@@ -41,5 +46,48 @@ public class WechatController {
         response.addHeader("x-token", accessToken.getAccess_token());
         return accessToken ;
     }
+
+
+    /**
+     * 重定向到网页授权地址
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping("/oauth2/authorization/wechat")
+    public void redirectLogin(HttpServletResponse response) throws IOException {
+        String callbackUrl = "https://www.yicj1.com/wechatLogin" ;
+        String encodeCallbackUrl = URLEncoder.encode(callbackUrl, "UTF-8") ;
+        String url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect" ;
+        response.sendRedirect(String.format(url, appId, encodeCallbackUrl));
+    }
+
+    /**
+     * {
+     *     "access_token": "45_hy_Cw5vPRFoWd6ra0cvLYNdN7Skto5_zhgl9FFQR51muPULaFAsHqIPgoRiPn62AI5ciyY10LasTPlqvyqoOdw",
+     *     "expires_in": 7200,
+     *     "refresh_token": "45_IAiui3UXfJm6Qwb6aWNaj4pTXn66CcGPA800Lro9vkmxweB3-RlRDqnaMyndALXlUFJrrpwngiJLzfkt3y8CIw",
+     *     "openid": "obC9S6seDG_rYlg90eAk7Qofw5-A",
+     *     "scope": "snsapi_userinfo"
+     * }
+     */
+    @GetMapping("/wechatLogin")
+    public String wechatLogin(String code){
+        String tokenUri = "https://api.weixin.qq.com/sns/oauth2/access_token" ;
+        String redirect_uri = "https://www.yicj1.com/wechatLogin" ;
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>() ;
+        params.set("appid", appId);
+        params.set("secret", appSecret);
+        params.set("code", code);
+        params.set("redirect_uri", redirect_uri);
+        params.set("grant_type", "authorization_code");
+        String tmpTokenResponse = this.restTemplate.postForObject(
+                tokenUri, params, String.class) ;
+        // 判断用户是否已经注
+        // 如果已经注册，db查询用户信息，并自动登录
+        // 如果为注册，则跳转到注册页面（获取微信用户信息），注册后并登录
+        // 登录成功以后跳转到对应页面
+        return tmpTokenResponse ;
+    }
+
 
 }
